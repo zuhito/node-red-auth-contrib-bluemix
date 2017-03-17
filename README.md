6c12b778d283c36aa75f26ea81f2aa7cf59ddfc6
zuhito <zuhito@hotmail.co.jp>
2017-03-18 02:00:48 +0900

    Modify bluemix-auth.js

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Authentication module for Node-RED on Bluemix
 
 Using this module on Bluemix, you can access to Node-RED by IBM ID.
+
+There is a setting example(bluemix-settings.js and package.json) on the following GitHub page.
+https://github.com/zuhito/node-red-bluemix-starter
--- bluemix-auth.js
+++ bluemix-auth.js
@@ -1,49 +1,55 @@
 var when = require("when");
-module.exports = {
-   type: "credentials",
-   users: function(username) {
-       return when.promise(function(resolve) {
-           if (true) {
-               var user = { username: username, permissions: "*" };
-               resolve(user);
-           } else {
-               resolve(null);
-           }
-       });
-   },
-   authenticate: function(username, password) {
-       return when.promise(function(resolve) {
-           var endpoint = "http://api.ng.bluemix.net";
-           var cc = new (require("cf-nodejs-client")).CloudController(endpoint);
-           var uaa = new (require("cf-nodejs-client")).UsersUAA;
-           var apps = new (require("cf-nodejs-client")).Apps(endpoint);
+var cf = require("cf-nodejs-client");
 
-           cc.getInfo().then( (result) => {
-               uaa.setEndPoint(result.authorization_endpoint);
-               return uaa.login(username, password);
-           }).then( (result) => {
-               apps.setToken(result);
-               return apps.getApps();
-           }).then( (result) => {
-               for (var i = 0; i < result.resources.length; i++)
-               {
-                   var name = require("cfenv").getAppEnv().app.application_name;
-                   if (result.resources[i].entity.name == name)
-                   {
-                       var user = { username: username, permissions: "*" };
-                       resolve(user);
-                   }
-               }
-               resolve(null);
-           }).catch( (reason) => {
-               console.error("Error: " + reason);
-               resolve(null);
-           });
-       });
-   },
-   default: function() {
-       return when.promise(function(resolve) {
-           resolve(null);
-       });
-   }
+module.exports = {
+    type: "credentials",
+    users: function(username) {
+        return when.promise(function(resolve) {
+            if (true) {
+                var user = { username: username, permissions: "*" };
+                resolve(user);
+            } else {
+                resolve(null);
+            }
+        });
+    },
+    authenticate: function(username, password) {
+        return when.promise(function(resolve) {
+            var endpoint = "http://api.ng.bluemix.net";
+            var cc = new cf.CloudController(endpoint);
+            var uaa = new cf.UsersUAA;
+            var apps = new cf.Apps(endpoint);
+
+            cc.getInfo().then( (result) => {
+                uaa.setEndPoint(result.authorization_endpoint);
+                return uaa.login(username, password);
+            }).then( (result) => {
+                apps.setToken(result);
+                return apps.getApps();
+            }).then( (result) => {
+                var flag = true;
+                for (var i = 0; i < result.resources.length && flag; i++)
+                {
+                    var name = require("cfenv").getAppEnv().app.application_name;
+                    if (result.resources[i].entity.name == name)
+                    {
+                        var user = { username: username, permissions: "*" };
+                        flag = false;
+                        resolve(user);
+                    }
+                }
+                if (flag) {
+                    resolve(null);
+                }
+            }).catch( (reason) => {
+                console.error("Error: " + reason);
+                resolve(null);
+            });
+        });
+    },
+    default: function() {
+        return when.promise(function(resolve) {
+            resolve(null);
+        });
+    }
 }
